09c4953b1f4f72deab00b17055b4f1d4b64c53d9
ThayseAda <thayse.frankenberger@gmail.com>
2024-09-09 15:47:45 -0300

    Update primeiro_teste.py

--- primeiro_teste.py
+++ primeiro_teste.py
@@ -1,8 +1,6 @@
 import subprocess
 
 def test_ola():
-    # Executar o script ola.py e capturar a saída
     result = subprocess.run(['python', 'ola.py'], capture_output=True, text=True)
     
-    # Verificar se a saída contém "Olá"
-    assert "Olá" in result.stdout
+    assert "Olá Thayse" in result.stdout
